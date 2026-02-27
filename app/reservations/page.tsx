8e4a98034153079132a8e955673c5737838fc4d6
CODED BLACK <codedb693@gmail.com>
2026-02-27 13:17:29 +0300

    Update page.tsx

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -28,6 +28,7 @@ const timeSlots = [
 
 const partySizes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10+"]
 
+
 export default function ReservationsPage() {
   const [formData, setFormData] = useState({
     name: "",
